ca9ac6396429e7ffd115f7d2494cee7e1038c5b6
Roshini V <roshini77783@gmail.com>
2026-09-11 11:44:39 +0530

    Delete pro1.py

--- pro1.py
+++
@@ -1,7 +0,0 @@
-num1 = float(input("Enter first number: "))
-num2 = float(input("Enter second number: "))
-
-if num1 > num2:
-    print("Largest number is:", num1)
-else:
-    print("Largest number is:", num2)
